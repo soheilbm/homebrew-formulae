7121984843c4a88f507e3ce611f87841d45f914b
Soheil B.Marvasti <soheilbm@me.com>
2018-03-12 15:31:55 +0800

    updating carhage cache version

--- Formula/carthage-cache.rb
+++ Formula/carthage-cache.rb
@@ -5,7 +5,7 @@
 class CarthageCache < Formula
   desc "A simple formula for caching carthage libraries"
   homepage "https://soheilbm.github.io/carthage-cache/"
-  url "https://github.com/soheilbm/carthage-cache.git", :tag => "v0.1.9"
+  url "https://github.com/soheilbm/carthage-cache.git", :tag => "v0.1.11"
   head "https://github.com/soheilbm/carthage-cache.git", :branch => "master"
   
   depends_on :xcode => ["8.0", :build]
